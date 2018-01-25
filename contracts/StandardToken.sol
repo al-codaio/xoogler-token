d87b1b22b5cfbf90a5dc04b8b2adc9ce3c5e2894
Al Chen <atc239@gmail.com>
2018-01-25 17:33:52 -0500

    rinkeby network deploy

--- contracts/StandardToken.sol
+++ contracts/StandardToken.sol
@@ -1,12 +1,3 @@
-/*
-This implements ONLY the standard functions and NOTHING else.
-For a token like you would want to deploy in something like Mist, see HumanStandardToken.sol.
-
-If you deploy this, you won't have anything useful.
-
-Implements ERC 20 Token standard: https://github.com/ethereum/EIPs/issues/20
-.*/
-
 pragma solidity ^0.4.4;
 
 import "./Token.sol";
@@ -14,10 +5,6 @@ import "./Token.sol";
 contract StandardToken is Token {
 
     function transfer(address _to, uint256 _value) returns (bool success) {
-        //Default assumes totalSupply can't be over max (2^256 - 1).
-        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
-        //Replace the if with this one instead.
-        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
         if (balances[msg.sender] >= _value && _value > 0) {
             balances[msg.sender] -= _value;
             balances[_to] += _value;
@@ -27,8 +14,6 @@ contract StandardToken is Token {
     }
 
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        //same as above. Replace this line with the following if you want to protect against wrapping uints.
-        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
         if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
             balances[_to] += _value;
             balances[_from] -= _value;
--- truffle.js
+++ truffle.js
@@ -1,11 +1,16 @@
 module.exports = {
-  "build": "webpack",
-  "deploy": [
-    "HumanStandardToken"
-  ],
-  "rpc": {
-    // Default RPC configuration.
-    "host": "localhost",
-    "port": 8545
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*"
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      from: "0xfb27816c324bb208fe630f5272e95e2d7487e287",
+      network_id: 4,
+      gas: 6000000
+    }
   }
 }
